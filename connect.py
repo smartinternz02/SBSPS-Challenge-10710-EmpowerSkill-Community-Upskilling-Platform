575ccadbbe2cddc1d7bfde679b27ac7454c6063e
28ananya <125064544+28ananya@users.noreply.github.com>
2023-09-04 16:55:18 +0530

    Add files via upload

---
+++ connect.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 15 13:58:50 2023
+
+@author: Shivani_SB
+"""
+def showall():
+    sql= "SELECT * from FIRST"
+    stmt = ibm_db.exec_immediate(conn, sql)
+    dictionary = ibm_db.fetch_both(stmt)
+    while dictionary != False:
+        print("The Name is : ",  dictionary["NAME"])
+        print("The E-mail is : ", dictionary["EMAIL"])
+        print("The Contact is : ",  dictionary["CONTACT"])
+        print("The Adress is : ",  dictionary["ADDRESS"])
+        print("The Role is : ",  dictionary["ROLE"])
+        print("The Branch is : ",  dictionary["BRANCH"])
+        print("The Password is : ",  dictionary["PASSWORD"])
+        dictionary = ibm_db.fetch_both(stmt)
+        
+def getdetails(email,password):
+    sql= "select * from FIRST where email='{}' and password='{}'".format(email,password)
+    stmt = ibm_db.exec_immediate(conn, sql)
+    dictionary = ibm_db.fetch_both(stmt)
+    while dictionary != False:
+        print("The Name is : ",  dictionary["NAME"])
+        print("The E-mail is : ", dictionary["EMAIL"])
+        print("The Contact is : ", dictionary["CONTACT"])
+        print("The Address is : ", dictionary["ADDRESS"])
+        print("The Role is : ", dictionary["ROLE"])
+        print("The Branch is : ", dictionary["BRANCH"])
+        print("The Password is : ", dictionary["PASSWORD"])
+        dictionary = ibm_db.fetch_both(stmt)
+        
+def insertdb(conn,name,email,contact,address,role,branch,password):
+    sql= "INSERT into FIRST VALUES('{}','{}','{}','{}','{}','{}','{}')".format(name,email,contact,address,role,branch,password)
+    stmt = ibm_db.exec_immediate(conn, sql)
+    print ("Number of affected rows: ", ibm_db.num_rows(stmt))
+
+try:
+    import ibm_db
+    conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=fbd88901-ebdb-4a4f-a32e-9822b9fb237b.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32731;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=dgn46090;PWD=nnE1pY9pb9oDWKCA",'','')
+    print(conn)
+    print("connection successful...")
+    insertdb(conn,"Hari","Hari@gmail.com",'1234567890','Adarsh nagar','Faculty','Civil','1234567')
+    getdetails("Hari@gmail.com",'1234567')
+    showall()
+
+except:
+    print("Error connecting to the database")
+
+
+
